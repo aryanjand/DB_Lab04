f596c0d7924a7495314105fadbe05dcb00a2f43d
Aryan Jand <97502222+aryanjand@users.noreply.github.com>
2023-02-06 14:56:30 -0800

    almost finised

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
-const express = require('express');
-const port = process.env.PORT || 3000;
-const app = express();
-app.use(express.urlencoded({ extended: false }));
 
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
 
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+const mongo_user = process.env.MONGODB_USER;;
+const mongo_password = process.env.MONGODB_PASSWORD;;
+const mongo_session_id = process.env.MONGODB_SESSION_SECR;
+const node_session_id = process.env.NODE_SESSION_SECRET;
+
+const expireTime = 1 * 60 * 60 * 1000
+
+// In memory databse
 let users = [];
+// users.push("123", bcrypt.hashSync("123", saltRounds));
+app.use(express.urlencoded({ extended: false }));
 
 
+let mongoStore = MongoStore.create({
+    mongoUrl: `mongodb+srv://${mongo_user}:${mongo_password}@comp3920.cgu2ibl.mongodb.net/?retryWrites=true&w=majority`,
+    crypto: {
+        secret: mongo_session_id
+    }
+});
+
+app.use(session({
+    secret: node_session_id,
+    store: mongoStore,
+    saveUninitialized: false,
+    resave: true
+}
+));
 
 app.get('/', (req, res) => {
     let html = `
-    <form action="/signup" method="post">
+    <form action="/signup" method="get">
         <button>Sign-Up</button>
     </form>
-    <form action="/login" method="post">
+    <form action="/login" method="get">
         <button>Log-In</button>
     </form>
     `
+    req.session.authenticated = false;
     res.send(html)
 });
-
-app.post('/signup', (req, res) => {
-    res.redirect('/signup')
-});
-
-app.post('/login', (req, res) => {
-    res.redirect('/login')
-})
 
 
 app.get('/signup', (req, res) => {
@@ -57,7 +78,7 @@ app.get('/login', (req, res) => {
     let missing = req.query.missing
     html_login = `
     Login User
-    <form action='/vaildate_user' method='post'>
+    <form action='/validate_user' method='post'>
         <input name="user_email" type="text" placeholder='Email'>
         <input name="user_password" type="password" placeholder="Password">
         <button>Submit</button>
@@ -68,29 +89,34 @@ app.get('/login', (req, res) => {
     } else if (missing == 2) {
         html_login += '<br/>Password is Required'
     }
+
     res.send(html_login)
 });
 
-app.post('/vaildate_user', (req, res) => {
+app.post('/validate_user', (req, res) => {
     let user_email = req.body.user_email
     let user_password = req.body.user_password
 
     if (!user_email) {
         res.redirect('/login?missing=1')
-    } else if (!user_password) {
+    }
+    else if (!user_password) {
         res.redirect('/login?missing=2')
     }
 
     for (i = 0; i < users.length; i++) {
         if (users[i].email == user_email) {
             if (bcrypt.compareSync(user_password, users[i].password)) {
+                req.session.authenticated = true;
+                req.session.user_email = user_email;
+                req.session.cookie.maxAge = expireTime;
+                // console.log("debug")
                 res.redirect("/members")
-                return;
             }
         }
     }
     //user and password combination not found
-    req.redirect("/")
+    // res.redirect("/")
 })
 
 app.post("/create_user", (req, res) => {
@@ -108,12 +134,29 @@ app.post("/create_user", (req, res) => {
         res.redirect("/signup?missing=3")
     }
     let hashedPassword = bcrypt.hashSync(password, saltRounds)
-    users.push({ name: name, email: email, password: hashedPassword })
-    res.redirect('/members')
+    users.push(
+        {
+            name: name,
+            email: email,
+            password: hashedPassword
+        }
+    )
+    console.log(users)
+    res.redirect('/')
 });
 
 app.get("/members", (req, res) => {
-    res.send('<h2>Welcome</h2>')
+    if (req.session.authenticated) {
+        html_members = `
+        <form>
+        <h2>Welcome ${req.se}</h2>
+        <button>Log Out</button>
+        </form>
+        `;
+        res.send(html_members);
+    } else {
+        res.redirect('/login');
+    }
 })
 
 
